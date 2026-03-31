494f5dc20cc24750d4d6a052d6c5deb5bd62f0c7
Vasilisa321 <wolpina206@gmail.com>
2026-03-31 15:16:36 +0700

    1st

---
+++ app/Controller/Site.php
@@ -0,0 +1,16 @@
+<?php
+namespace Controller;
+
+class Site
+{
+    public function index(): string
+    {
+        $view = new View();
+        return $view->render('site.hello', ['message' => 'index working']);
+    }
+
+    public function hello(): string
+    {
+        return new View('site.hello', ['message' => 'hello working']);
+    }
+}
